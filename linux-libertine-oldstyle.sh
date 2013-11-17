e5a5267a9d1fa0da113f7ac826d95e74290bb106
maieul <maieul@maieul.net>
2013-11-17 17:59:40 +0100

    mettre le OldStyle Number ailleurs

--- linux-libertine-oldstyle.sh
+++ linux-libertine-oldstyle.sh
@@ -13,7 +13,7 @@ mv *otf bak
 for i in *.ttx;
   do 
 sed -i ttx 's#<map code="\(0x3[0-9]\)" name="\([a-z]*\)"/>#<map code="\1" name="\2.oldstyle"/>#' $i;# correction
-sed -i ttx "s/Linux Libertine O/Linux Libertine OldStyleNumber O/" $i;
+sed -i ttx "s/Linux Libertine/Linux Libertine OldStyleNumber/" $i;
 sed -i ttx 's#<Ligature components="" glyph="slash"/>##g' $i;#bug temporaire
 ttx $i;
 mv ${i%%.ttx}.ttf ${i%%.ttx}.otf;
