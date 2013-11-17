53b422aa6fabec7fad7c92d6444cb2a417669da2
maieul <maieul@maieul.net>
2013-11-17 18:11:49 +0100

    mettre en .ttxbak les sauvegarde des .ttx

--- linux-libertine-oldstyle.sh
+++ linux-libertine-oldstyle.sh
@@ -1,6 +1,6 @@
 # nettoyage
 rm -f *ttx
-rm -f *ttf
+rm -f *.ttf*
 
 #transformation
 ttx *otf
@@ -10,11 +10,11 @@ mkdir -p bak
 mv *otf bak
 
 #correction des ttx et generation des .otf
-for i in *.ttx;
+for i in *ttx;
   do 
-sed -i ttx 's#<map code="\(0x3[0-9]\)" name="\([a-z]*\)"/>#<map code="\1" name="\2.oldstyle"/>#' $i;# correction
-sed -i ttx "s/Linux Libertine/Linux Libertine OldStyleNumber/" $i;
-sed -i ttx 's#<Ligature components="" glyph="slash"/>##g' $i;#bug temporaire
+sed -i .ttxbak 's#<map code="\(0x3[0-9]\)" name="\([a-z]*\)"/>#<map code="\1" name="\2.oldstyle"/>#' $i;# correction
+sed -i .ttxbak "s/Linux Libertine/Linux Libertine OldStyleNumber/" $i;
+sed -i .ttxbak  's#<Ligature components="" glyph="slash"/>##g' $i;#bug temporaire
 ttx $i;
 mv ${i%%.ttx}.ttf ${i%%.ttx}.otf;
 done
