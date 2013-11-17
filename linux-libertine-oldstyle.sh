52cd4a82cbe9e2989ce60c792e49140e122ff33e
maieul <maieul@maieul.net>
2013-11-17 18:31:24 +0100

    boucler correctement sur les fichiers + vérifier qu'il y a bien des nombre en oldstyle

--- linux-libertine-oldstyle.sh
+++ linux-libertine-oldstyle.sh
@@ -10,11 +10,15 @@ mkdir -p bak
 mv *otf bak
 
 #correction des ttx et generation des .otf
-for i in *ttx;
+for i in $(ls *ttx);
   do 
-sed -i .ttxbak 's#<map code="\(0x3[0-9]\)" name="\([a-z]*\)"/>#<map code="\1" name="\2.oldstyle"/>#' $i;# correction
-sed -i .ttxbak "s/Linux Libertine/Linux Libertine OldStyleNumber/" $i;
-sed -i .ttxbak  's#<Ligature components="" glyph="slash"/>##g' $i;#bug temporaire
-ttx $i;
-mv ${i%%.ttx}.ttf ${i%%.ttx}.otf;
+echo $i;
+if $(grep -q oldstyle $i) ;#uniquement si on on a des nombres à l'ancienne
+ then
+  sed -i .ttxbak 's#<map code="\(0x3[0-9]\)" name="\([a-z]*\)"/>#<map code="\1" name="\2.oldstyle"/>#' $i;# correction
+  sed -i .ttxbak "s/Linux Libertine/Linux Libertine OldStyleNumber/" $i;
+  sed -i .ttxbak  's#<Ligature components="" glyph="slash"/>##g' $i;#bug temporaire
+ ttx $i;
+ mv ${i%%.ttx}.ttf ${i%%.ttx}.otf;
+fi
 done
