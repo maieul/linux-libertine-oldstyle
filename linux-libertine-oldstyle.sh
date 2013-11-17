40b315a2dac6f5f2c972b4641e35812a95d1a079
maieul <maieul@maieul.net>
2013-11-17 16:55:03 +0100

    changer aussi le nom, pour eviter toute confusion

--- linux-libertine-oldstyle.sh
+++ linux-libertine-oldstyle.sh
@@ -12,7 +12,8 @@ mv *otf bak
 #correction des ttx et generation des .otf
 for i in *.ttx;
   do 
-sed -i ttx 's#<map code="\(0x3[0-9]\)" name="\([a-z]*\)"/>#<map code="\1" name="\2.oldstyle"/>#' *ttx;
+sed -i ttx 's#<map code="\(0x3[0-9]\)" name="\([a-z]*\)"/>#<map code="\1" name="\2.oldstyle"/>#' $i;# correction
+sed -i ttx "s/Linux Libertine O/Linux Libertine OldStyleNumber O/" $i;
 ttx $i;
 mv ${i%%.ttx}.ttf ${i%%.ttx}.otf;
 done
