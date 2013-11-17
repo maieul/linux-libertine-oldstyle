7d2c2e66db3d545f5ff1385b9d397e649654eb67
maieul <maieul@maieul.net>
2013-11-17 17:08:35 +0100

    correction d'un bug, esperons temporaire, sur certaines variante de police

--- linux-libertine-oldstyle.sh
+++ linux-libertine-oldstyle.sh
@@ -14,6 +14,7 @@ for i in *.ttx;
   do 
 sed -i ttx 's#<map code="\(0x3[0-9]\)" name="\([a-z]*\)"/>#<map code="\1" name="\2.oldstyle"/>#' $i;# correction
 sed -i ttx "s/Linux Libertine O/Linux Libertine OldStyleNumber O/" $i;
+sed -i ttx 's#<Ligature components="" glyph="slash"/>##g' $i;#bug temporaire
 ttx $i;
 mv ${i%%.ttx}.ttf ${i%%.ttx}.otf;
 done
